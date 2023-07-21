3c233bb0fd2ad27ce6e8b39ad83a4a00d868e7c8
Saksham Mittal <gotlouemail@gmail.com>
2023-07-21 13:14:36 +0530

    Process SQL output into a better struct
    Also change schema to not store cipher type,
    since this seems redundant right now and can
    be added back in if needed

--- src/db.rs
+++ src/db.rs
@@ -1,32 +1,51 @@
 use rusqlite::{params, Connection, Result};
+use russh_keys::{key::KeyPair, PublicKeyBase64};
 use std::fmt::Display;
 
 /// Row representation
-pub(crate) struct StructuredKey {
+struct RawKey {
     nickname: String,
     user: String,
     host: String,
     port: u16,
     encoded_key: Vec<u8>,
-    cipher: String,
 }
 
-impl StructuredKey {
-    pub fn get_key(&self) -> &[u8] {
-        &self.encoded_key
-    }
-    pub fn get_conn_info(&self) -> (&str, &str, u16) {
-        (&self.user, &self.host, self.port)
+pub(crate) struct ProcessedKey {
+    pub nickname: String,
+    pub user: String,
+    pub host: String,
+    pub port: u16,
+    pub keypair: KeyPair,
+    pub pubkey: String,
+    pub cipher: String,
+}
+
+impl RawKey {
+    pub fn process(&self) -> ProcessedKey {
+        let keypair = russh_keys::pkcs8::decode_pkcs8(&self.encoded_key, None).unwrap();
+        let pubkey = keypair.clone_public_key().unwrap().public_key_base64();
+        let cipher = keypair.name().to_string();
+        ProcessedKey {
+            nickname: self.nickname.clone(),
+            user: self.user.clone(),
+            host: self.host.clone(),
+            port: self.port,
+            keypair,
+            pubkey,
+            cipher,
+        }
     }
 }
 
-impl Display for StructuredKey {
+impl Display for ProcessedKey {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         writeln!(
             f,
-            "SSH key '{}' using cipher '{}'",
-            self.nickname, self.cipher
+            "SSH key '{}' for {}@{}:{} using cipher '{}'",
+            self.nickname, self.user, self.host, self.port, self.cipher
         )?;
+        writeln!(f, "{} {} {}", self.cipher, self.pubkey, self.nickname)?;
         Ok(())
     }
 }
@@ -41,12 +60,10 @@ pub(crate) fn open_db() -> Result<Connection> {
                     user VARCHAR,
                     host VARCHAR,
                     port INT,
-                    encoded_key BLOB,
-                    cipher VARCHAR
+                    encoded_key BLOB
                 )",
                 [],
-            )
-            .unwrap();
+            )?;
             Ok(conn)
         }
         Err(e) => Err(e),
@@ -59,45 +76,37 @@ pub(crate) fn insert_key<'a>(
     host: &str,
     port: u16,
     encoded_key: Vec<u8>,
-    cipher: &str,
 ) -> bool {
     let db = open_db().unwrap();
     let mut prepstatement = db
         .prepare(
             "INSERT INTO keys (
-        nickname, user, host, port, encoded_key, cipher
-        ) VALUES (?1, ?2, ?3, ?4, (?5), ?6)",
+        nickname, user, host, port, encoded_key
+        ) VALUES (?1, ?2, ?3, ?4, (?5))",
         )
         .unwrap();
-    match prepstatement.execute(params![
-        nick,
-        user,
-        host,
-        &port.to_string(),
-        encoded_key,
-        cipher
-    ]) {
+    match prepstatement.execute(params![nick, user, host, &port.to_string(), encoded_key]) {
         Ok(_) => true,
         Err(_) => false,
     }
 }
 
 /// Get the required key from the database
-pub(crate) fn get_key(nick: &str) -> Result<StructuredKey> {
+pub(crate) fn get_key(nick: &str) -> Result<ProcessedKey> {
     let db = open_db()?;
     let mut prepstatement = db.prepare(
         "SELECT 
-        nickname, user, host, port, encoded_key, cipher
+        nickname, user, host, port, encoded_key
         FROM keys WHERE nickname = ?1",
     )?;
     prepstatement.query_row([nick], |row| {
-        Ok(StructuredKey {
+        let row = RawKey {
             nickname: row.get(0)?,
             user: row.get(1)?,
             host: row.get(2)?,
             port: row.get(3)?,
             encoded_key: row.get(4)?,
-            cipher: row.get(5)?,
-        })
+        };
+        Ok(row.process())
     })
 }
