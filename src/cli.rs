0a1d9c0b981abe41de5033e21e0a685d152facbe
Saksham Mittal <gotlouemail@gmail.com>
2023-07-21 13:39:31 +0530

    Break out CLI args into different module

---
+++ src/cli.rs
@@ -0,0 +1,62 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Subcommand, PartialEq)]
+pub enum Command {
+    /// Generate a key for a particular service
+    GenKey {
+        /// key nickname
+        #[arg(required = true)]
+        name: String,
+        /// Username
+        #[arg(required = true)]
+        user: String,
+        /// Hostname
+        #[arg(required = true)]
+        host: String,
+        /// Port (optional)
+        #[arg(short, long, default_value_t = 22)]
+        port: u16,
+    },
+    /// Show a key for a particular service,
+    /// If no name is given, return all keys
+    ShowKey {
+        /// key nickname
+        #[arg(required = false)]
+        name: Option<String>,
+    },
+    /// Delete a key for a particular service
+    DeleteKey {
+        /// key nickname
+        #[arg(required = true)]
+        name: String,
+    },
+    /// Create a new key for a pre-existing service
+    UpdateKey {
+        /// key nickname
+        #[arg(required = true)]
+        name: String,
+        /// Username
+        #[arg(long, required = false)]
+        user: Option<String>,
+        /// Hostname
+        #[arg(long, required = false)]
+        host: Option<String>,
+        /// Port (optional)
+        #[arg(long, required = false)]
+        port: Option<u16>,
+    },
+    /// Connect to a service using its nickname
+    Connect {
+        /// key nickname
+        #[arg(required = true)]
+        name: String,
+    },
+}
+
+/// Rust reimplementation of SSH client with sandboxing
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+pub(crate) struct Args {
+    #[command(subcommand)]
+    pub(crate) command: Option<Command>,
+}
