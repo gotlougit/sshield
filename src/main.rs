c43d23f4c339063d0e171a116f0cb14c7fda4702
Saksham Mittal <gotlouemail@gmail.com>
2023-07-23 21:36:40 +0530

    Use modified input API in main.rs

--- src/main.rs
+++ src/main.rs
@@ -86,14 +86,14 @@ impl KeyMgr {
                         .await
                         .unwrap();
                     channel.request_shell(false).await.unwrap();
+                    crossterm::terminal::enable_raw_mode().unwrap();
                     loop {
-                        let mut inputval = crate::input::get_line();
+                        let inputval = crate::input::get_line(&mut channel).await;
                         if inputval == "exit" {
+                            crossterm::terminal::disable_raw_mode().unwrap();
                             channel.close().await.unwrap();
                             return;
                         }
-                        inputval += "\n";
-                        channel.data(inputval.as_bytes()).await.unwrap();
                     }
                 }
             }
