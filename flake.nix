bfd62180ca40302f59ee74c80c9b9f0b3e3495a2
Saksham Mittal <gotlouemail@gmail.com>
2023-07-26 23:40:47 +0530

    Partially rewrite flake for egui

--- flake.nix
+++ flake.nix
@@ -1,20 +1,35 @@
 {
-  outputs = { self, nixpkgs }:
-    let
-      system = "x86_64-linux";
-      pkgs = import nixpkgs { inherit system; };
-      mkShell = pkgs.mkShell.override { stdenv = pkgs.stdenvAdapters.useMoldLinker pkgs.stdenv; };
-    in {
-       defaultPackage.${system} = mkShell {
-        name = "rustdev";
-        buildInputs = [
-          pkgs.pkgconfig
-          pkgs.openssl.dev
-          pkgs.cargo
-          pkgs.rustc
-          pkgs.rustfmt
-          pkgs.rust-analyzer
-        ];
-      };
-    };
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        mkShell = pkgs.mkShell.override { stdenv = pkgs.stdenvAdapters.useMoldLinker pkgs.stdenv; };
+      in with pkgs; rec {
+        devShell = mkShell rec {
+          buildInputs = [
+            pkgconfig
+            libxkbcommon
+            libGL
+
+            # WINIT_UNIX_BACKEND=wayland
+            wayland
+
+            # WINIT_UNIX_BACKEND=x11
+            xorg.libXcursor
+            xorg.libXrandr
+            xorg.libXi
+            xorg.libX11
+            openssl.dev
+            cargo
+            rustc
+            rustfmt
+            rust-analyzer
+          ];
+          LD_LIBRARY_PATH = "${lib.makeLibraryPath buildInputs}";
+        };
+      });
 }
