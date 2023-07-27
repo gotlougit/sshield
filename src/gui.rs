fce607b6db7bd346812cd2282edd5208fc398ff1
Saksham Mittal <gotlouemail@gmail.com>
2023-07-27 11:28:55 +0530

    Initial gui.rs code to create confirm window

--- src/gui.rs
+++ src/gui.rs
@@ -1,37 +1,12 @@
-use eframe::egui;
+use dialog::{Choice, DialogBox};
 
-pub fn confirm_window(msg: String) {
-    let mut native_options = eframe::NativeOptions::default();
-    native_options.initial_window_size = Some(eframe::egui::Vec2::new(300.0, 200.0));
-    eframe::run_native(
-        "sshield",
-        native_options,
-        Box::new(|cc| Box::new(ConfirmWindowApp::new(cc, msg))),
-    )
-    .unwrap();
-}
-
-#[derive(Default)]
-struct ConfirmWindowApp {
-    msg: String,
-}
-
-impl ConfirmWindowApp {
-    fn new(_cc: &eframe::CreationContext<'_>, msg: String) -> Self {
-        Self { msg }
-    }
-}
-
-impl eframe::App for ConfirmWindowApp {
-    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-        egui::CentralPanel::default().show(ctx, |ui| {
-            ui.heading("{self.msg}");
-            if ui.button("Yes").clicked() {
-                return;
-            }
-            if ui.button("No").clicked() {
-                return;
-            }
-        });
+pub fn confirm_request(msg: &str) -> bool {
+    let choice = dialog::Question::new(msg)
+        .title("sshield")
+        .show()
+        .unwrap_or(Choice::No);
+    match choice {
+        Choice::Yes => true,
+        _ => false,
     }
 }
