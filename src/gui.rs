2d86c4ccaabda09d416c744eb480e3b2311eabb1
Saksham Mittal <gotlouemail@gmail.com>
2023-07-31 14:25:27 +0530

    Change titles and messages in GUI

--- src/gui.rs
+++ src/gui.rs
@@ -2,7 +2,7 @@ use dialog::{Choice, DialogBox};
 
 pub fn confirm_request(msg: &str) -> bool {
     let choice = dialog::Question::new(msg)
-        .title("sshield")
+        .title("sshield - Confirm request")
         .show()
         .unwrap_or(Choice::No);
     match choice {
@@ -13,7 +13,7 @@ pub fn confirm_request(msg: &str) -> bool {
 
 pub fn get_db_pass() -> String {
     dialog::Password::new("Enter database password: ")
-        .title("Password")
+        .title("sshield - Unlock database")
         .show()
         .unwrap()
         .unwrap_or(String::new())
@@ -21,7 +21,7 @@ pub fn get_db_pass() -> String {
 
 pub fn get_new_db_pass() -> String {
     dialog::Password::new("Enter new database password: ")
-        .title("sshield- Change password")
+        .title("sshield - Change password")
         .show()
         .unwrap()
         .unwrap_or(String::new())
