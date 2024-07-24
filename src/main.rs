2120f977ca4c27d7319f24092770c39ec36b566b
Saksham Mittal <git@gotlou.com>
2024-07-24 22:09:08 +0530

    fix: error out when empty password is found

--- src/main.rs
+++ src/main.rs
@@ -66,84 +66,88 @@ async fn main() {
     let args = Args::parse();
     if let Some(cmd) = args.command {
         let pass = crate::config::get_pass(trust_keyring).unwrap();
-        match Client::init(pass.as_str(), &db_path) {
-            Ok(mgr) => {
-                match cmd {
-                    Command::ChangePassword {} => {
-                        std::mem::drop(mgr);
-                        let newpass = crate::gui::get_new_db_pass();
-                        match crate::db::change_db_pass(&db_path, &pass, &newpass) {
-                            Ok(_) => println!("Password of database changed successfully!"),
-                            Err(_) => eprintln!("Password was unable to be changed!"),
-                        };
-                    }
-                    Command::GenKey {
-                        name,
-                        user,
-                        host,
-                        port,
-                    } => {
-                        match mgr.gen_key(&name, &user, &host, port).await {
-                            true => println!("{}", mgr.show_key(&name).unwrap()),
-                            false => println!("Error, key wasn't able to be created"),
-                        };
-                    }
-                    Command::ShowKey { name } => match name {
-                        Some(name) => {
-                            mgr.show_key(&name).unwrap();
+        if !pass.is_empty() {
+            match Client::init(pass.as_str(), &db_path) {
+                Ok(mgr) => {
+                    match cmd {
+                        Command::ChangePassword {} => {
+                            std::mem::drop(mgr);
+                            let newpass = crate::gui::get_new_db_pass();
+                            match crate::db::change_db_pass(&db_path, &pass, &newpass) {
+                                Ok(_) => println!("Password of database changed successfully!"),
+                                Err(_) => eprintln!("Password was unable to be changed!"),
+                            };
                         }
-                        None => {
-                            let keys = mgr.show_all_keys();
-                            for key in keys.iter() {
-                                println!("{key}");
+                        Command::GenKey {
+                            name,
+                            user,
+                            host,
+                            port,
+                        } => {
+                            match mgr.gen_key(&name, &user, &host, port).await {
+                                true => println!("{}", mgr.show_key(&name).unwrap()),
+                                false => println!("Error, key wasn't able to be created"),
+                            };
+                        }
+                        Command::ShowKey { name } => match name {
+                            Some(name) => {
+                                mgr.show_key(&name).unwrap();
+                            }
+                            None => {
+                                let keys = mgr.show_all_keys();
+                                for key in keys.iter() {
+                                    println!("{key}");
+                                }
+                            }
+                        },
+                        Command::DeleteKey { name } => match mgr.delete_key(name.as_str()) {
+                            true => println!("Key deleted"),
+                            false => println!("Key could not be deleted, maybe it does not exist?"),
+                        },
+                        Command::Serve {} => {
+                            tokio::spawn(async move {
+                                tokio::signal::ctrl_c().await.unwrap();
+                                println!("Closing socket...");
+                                socket::close_socket().await;
+                                exit(0);
+                            });
+                            println!("Starting server process...");
+                            let task1 = socket::start_server(prompt);
+                            let task2 = {
+                                // In order to prevent any funny business with the scheduler,
+                                // let's wait for a while and let the other task take over
+                                tokio::time::sleep(std::time::Duration::from_secs(1)).await;
+                                mgr.add_all_keys()
+                            };
+                            tokio::join!(task1, task2);
+                        }
+                        Command::UpdateKey {
+                            name,
+                            user,
+                            host,
+                            port,
+                            genkey,
+                        } => {
+                            match mgr.update_key(&name, &user, &host, &port, &genkey) {
+                                true => println!("Updated key successfully"),
+                                false => eprintln!("Couldn't update key, does it exist?"),
+                            };
+                        }
+                        Command::ImportKey { name, path, pass } => {
+                            match mgr.import_key_from_file(pass, &name, &path).await {
+                                true => println!("Key imported successfully"),
+                                false => eprintln!("Couldn't import key, check path or config?"),
                             }
                         }
-                    },
-                    Command::DeleteKey { name } => match mgr.delete_key(name.as_str()) {
-                        true => println!("Key deleted"),
-                        false => println!("Key could not be deleted, maybe it does not exist?"),
-                    },
-                    Command::Serve {} => {
-                        tokio::spawn(async move {
-                            tokio::signal::ctrl_c().await.unwrap();
-                            println!("Closing socket...");
-                            socket::close_socket().await;
-                            exit(0);
-                        });
-                        println!("Starting server process...");
-                        let task1 = socket::start_server(prompt);
-                        let task2 = {
-                            // In order to prevent any funny business with the scheduler,
-                            // let's wait for a while and let the other task take over
-                            tokio::time::sleep(std::time::Duration::from_secs(1)).await;
-                            mgr.add_all_keys()
-                        };
-                        tokio::join!(task1, task2);
-                    }
-                    Command::UpdateKey {
-                        name,
-                        user,
-                        host,
-                        port,
-                        genkey,
-                    } => {
-                        match mgr.update_key(&name, &user, &host, &port, &genkey) {
-                            true => println!("Updated key successfully"),
-                            false => eprintln!("Couldn't update key, does it exist?"),
-                        };
-                    }
-                    Command::ImportKey { name, path, pass } => {
-                        match mgr.import_key_from_file(pass, &name, &path).await {
-                            true => println!("Key imported successfully"),
-                            false => eprintln!("Couldn't import key, check path or config?"),
-                        }
-                    }
-                };
-            }
-            Err(_) => {
-                crate::config::delete_pass_from_keyring().unwrap();
-                eprintln!("Check database password or location, unable to open database");
-            }
-        };
+                    };
+                }
+                Err(_) => {
+                    crate::config::delete_pass_from_keyring().unwrap();
+                    eprintln!("Check database password or location, unable to open database");
+                }
+            };
+        } else {
+            eprintln!("Password is empty!");
+        }
     };
 }
